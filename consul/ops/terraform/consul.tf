33dcff7cd89cc901773d054cb3ce237290c60f51
Kevin Fishner <KFishner@gmail.com>
2015-02-17 17:58:42 -0800

    consul commit one

---
+++ consul/ops/terraform/consul.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+    access_key = "YOUR_ACCESS_KEY_HERE"
+    secret_key = "YOUR_SECRET_KEY_HERE"
+    region = "us-east-1"
+}
+
+resource "atlas_artifact" "consul" {
+    name = "<username>/consul"
+    type = "aws.ami"
+}
+
+resource "aws_instance" "consul" {
+    instance_type = "t2.small"
+    ami = "${atlas_artifact.consul.metadata_full.region-us-east-1}"
+
+    # This will create 3 instances
+    count = 3
+}
