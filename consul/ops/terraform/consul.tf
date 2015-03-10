dfe670512667cbce695d01a64ed641ba4397b090
Kevin Fishner <KFishner@gmail.com>
2015-03-10 16:29:39 -0700

    Update consul tf to build inside a vpc

--- consul/ops/terraform/consul.tf
+++ consul/ops/terraform/consul.tf
@@ -12,7 +12,21 @@ resource "atlas_artifact" "consul" {
 resource "aws_instance" "consul" {
     instance_type = "t2.micro"
     ami = "${atlas_artifact.consul.metadata_full.region-us-east-1}"
+    subnet_id = "${aws_subnet.main.id}"
 
     # This will create 3 instances
     count = 3
+}
+
+resource "aws_subnet" "main" {
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block = "10.0.1.0/24"
+
+    tags {
+        Name = "Main"
+    }
+}
+
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/16"
 }
