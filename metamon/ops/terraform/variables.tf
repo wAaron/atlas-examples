b8a0cfd42ab29ddd4fee880e030a4d179c081320
Jon Benson <bensojon@gmail.com>
2015-04-06 11:09:08 -0700

    Update tf vars comment

--- metamon/ops/terraform/variables.tf
+++ metamon/ops/terraform/variables.tf
@@ -2,11 +2,11 @@
 referencing environment variables, or you can use
 a terraform.tfvars file, see below example
 
-terraform plan \
-    -var 'atlas_username=${ATLAS_USERNAME}' \
-    -var 'atlas_token=${ATLAS_TOKEN}' \
-    -var 'aws_access_key=${AWS_SECRET_KEY}' \
-    -var 'aws_secret_key=${AWS_SECRET_KEY}'
+terraform apply \
+    -var "atlas_username=${ATLAS_USERNAME}" \
+    -var "atlas_token=${ATLAS_TOKEN}" \
+    -var "aws_access_key=${AWS_ACCESS_KEY}" \
+    -var "aws_secret_key=${AWS_SECRET_KEY}"
 
 See https://www.terraform.io/intro/getting-started/variables.html */
 
