e0c68c2823014189f8d0d203ca0ff1016d83cf6d
Jon Benson <bensojon@gmail.com>
2015-04-06 10:25:44 -0700

    Update TF var files, gitignore, and consul infra conf

--- metamon/ops/terraform/terraform.tfvars
+++ metamon/ops/terraform/terraform.tfvars
@@ -1,3 +1,8 @@
+/* You can either use this terraform.tfvars file to pass variables
+to terraform and ignore it from version control, or just
+pass them in through the command line as noted in variables.tf
+See https://www.terraform.io/intro/getting-started/variables.html */
+
 atlas_username = "YOUR_ATLAS_USERNAME"
 atlas_token = "YOUR_ATLAS_TOKEN"
 aws_access_key = "YOUR_AWS_ACCESS_KEY"
--- metamon/ops/terraform/variables.tf
+++ metamon/ops/terraform/variables.tf
@@ -1,3 +1,15 @@
+/* These can be passed in through the command line by
+referencing environment variables, or you can use
+a terraform.tfvars file, see below example
+
+terraform plan \
+    -var 'atlas_username=${ATLAS_USERNAME}' \
+    -var 'atlas_token=${ATLAS_TOKEN}' \
+    -var 'aws_access_key=${AWS_SECRET_KEY}' \
+    -var 'aws_secret_key=${AWS_SECRET_KEY}'
+
+See https://www.terraform.io/intro/getting-started/variables.html */
+
 variable "atlas_username" {}
 variable "atlas_token" {}
 variable "aws_access_key" {}
